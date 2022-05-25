c1cda1ba0ba6923ee8d000e5fe684d2819612834
Gideon Landeman <gideonlandeman@gmail.com>
2022-05-25 19:24:13 +0200

    Complete combTest.sh file.
    Signed-off-by: Gideon Landeman <gideonlandeman@gmail.com>

--- combTest.sh
+++ combTest.sh
@@ -1,35 +1,43 @@
 #! /bin/bash
-sudo apt-get update
+apt-get update
 apt-get install bc
 
-N=5 #number of iterations
+N=$1 #number of iterations
 c=0 #counter
 totalRuntimeLaunch=0
 totalRuntimeStartStop=0
 averageRuntimeLaunch=0
 averageRuntimeStartStop=0
 
+docker run -t -d --name t1 $2
+docker stop -t 0 t1
+docker rm t1
+
+
 for ((n=0;n<$N;n++))
 do
 	startL=$(date +%s.%6N)
-	docker run -t -d --name t1 -p 8080:80  nginx
+	docker run -t -d --name t1 -p 8080:80 $2 
 	until $(curl --output /dev/null --silent --head --fail localhost:8080); do
 		:
 	done
 	endL=$(date +%s.%6N)
 
-    startSS=$(date +%s.%6N)
-	docker stop t1
-	docker start -p 8080:80 t1
-	until $(curl --output /dev/null --silent --head --fail localhost:8080); do
+    	startSS=$(date +%s.%6N)
+	docker stop -t 0 t1
+	docker start -p 8090:80 t1
+	until $(curl --output /dev/null --silent --head --fail localhost:8090); do
 		:
 	done
 	endSS=$(date +%s.%6N)
 
 	runtimeLaunch=$(echo "$endL - $startL" | bc -l)
 	runtimeStartStop=$(echo "$endSS - $startSS" | bc -l)
-	#echo "runtime LAUNCH: $runtimeLaunch"
-	#echo "runtime START-STOP: $runtimeStartStop"
+	echo ""
+	echo ""
+	echo "iteration: $n"	
+	echo "runtime LAUNCH: $runtimeLaunch"
+	echo "runtime START-STOP: $runtimeStartStop"
 
 	totalRuntimeLaunch=$(echo "$runtimeLaunch + $totalRuntimeLaunch" | bc -l)
 	totalRuntimeStartStop=$(echo "$runtimeStartStop + $totalRuntimeStartStop" | bc -l)
@@ -37,7 +45,7 @@ do
 	#echo "toalRuntime START-STOP: $totalRuntimeStartStop"
 
 	##cleanup
-	docker stop t1
+	docker stop -t 0 t1
 	docker rm t1
 done
 averageRuntimeLaunch=$(echo "$totalRuntimeLaunch / $N" | bc -l)
